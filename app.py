48202cca9afa39cde495e6845de42db9517dc451
ArgyPorgy <carghya10@gmail.com>
2023-12-09 02:42:53 +0530

    .

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/userpage.html')
+def user():
+    return render_template('userpage.html')
+
+@app.route('/orgpage.html')
+def org():
+    return render_template('orgpage.html')
+
 if __name__ == '__main__':
     app.run(debug=True,port = 8000)
