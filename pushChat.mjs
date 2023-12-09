401a489eabf88f3e20410b9474656e67a849de49
Sayan Genri <sayangenri1@gmail.com>
2023-12-10 00:31:46 +0530

    .

--- pushChat.mjs
+++ pushChat.mjs
@@ -1,20 +1,19 @@
 import { CONSTANTS, PushAPI } from '@pushprotocol/restapi';
-import { EVENTS, createSocketConnection } from '@pushprotocol/socket';
 import { ethers } from 'ethers';
 
 // Creating a random signer from a wallet, ideally this is the wallet you will connect
-
+//user
 const privetkey = "980faad6ce73dd1be6d847fadee6a20e0c9137b237b0ccea5363f629a7a7ac74";
 const signerAlice = new ethers.Wallet(`0x${privetkey}`);
 
-
+//org
 const signerBobAddress = "964836a879a9301e17fc9f7514df85e4d408e1bf978cc56d4093f40bb958a3c9";
 const signerBob = new ethers.Wallet(`0x${signerBobAddress}`);
 
 
 // Send a message to Bob
 const aliceMessagesBob = await signerAlice.chat.send(bobWalletAddress, {
-  content: "Gm gm! It's a me... Mario",
+  content: "I need help !! ",
 });
 
 // Initialize Stream
@@ -135,4 +134,4 @@ stream.connect();
 
 // // Events and methods for socket connection are set, now connect stream
 // await streamAlice.connect();
-// await streamBob.connect();
+// await streamBob.connect();*/
