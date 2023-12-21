b04e4e7b959c282e9d8a91c4d02ce2872a51c496
Sayan Genri <137492664+Sayangenri@users.noreply.github.com>
2023-12-21 07:31:09 +0530

    Update pushNoti.mjs
    Hide Privet key & Address

--- pushNoti.mjs
+++ pushNoti.mjs
@@ -3,11 +3,11 @@ import { ethers } from "ethers";
 // Creating a random signer from a wallet, ideally this is the wallet you will connect
 
 async function pushCall(){
-const privetkey = "c5df9f4535944d8e3df7b5801e5552a9974f60b565af17b4e9b2ace1442308ce";
+const privetkey = "";
 const signer = new ethers.Wallet(`0x${privetkey}`);
 const userAlice = await PushAPI.initialize(signer, { env: CONSTANTS.ENV.STAGING });
 // Push channel address
-const pushChannelAdress = "0x998F8Fca5845908E83FFe299b98eC3F5c05b3093";
+const pushChannelAdress = "";
 // Subscribe to push channel
 await userAlice.notification.subscribe(
   `eip155:11155111:${pushChannelAdress}`,
